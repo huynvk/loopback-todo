41ca5a82729302c366280b677d5acf62fc69c726
Huy Ngo <huy.ngo@codelink.io>
2021-05-06 13:31:06 +0700

    Add relation

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -20,6 +20,10 @@ export class Todo extends Entity {
   })
   isComplete?: boolean;
 
+  @property({
+    type: 'number',
+  })
+  todoListId?: number;
 
   constructor(data?: Partial<Todo>) {
     super(data);
